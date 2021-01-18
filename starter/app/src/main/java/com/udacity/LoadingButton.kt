38e82edf0c01447afccf08ce4e8b8a34b5597deb
Ivan Mang <ivanmang1997@gmail.com>
2021-01-18 17:07:59 +0800

    Added loading button

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,8 +3,12 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -13,20 +17,38 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
+    private var buttonColor = 0
+    private var label = ""
+
     private val valueAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create( "", Typeface.BOLD)
+    }
+
 
     init {
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            buttonColor = getColor(R.styleable.LoadingButton_buttonColor, 0)
+            label = getString(R.styleable.LoadingButton_text).toString()
+        }
 
     }
 
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        paint.color = buttonColor
+        canvas.drawRect(0F, 0F, width.toFloat(), height.toFloat(), paint)
+        paint.color = Color.BLACK
+        canvas.drawText(label, 0, label.length, width.toFloat()/2, (height.toFloat() + 16)/2, paint)
 
     }
 
