e58252a36ded126d1ac349deb60d6e008530faaa
Ivan Mang <ivanmang1997@gmail.com>
2021-01-24 19:57:26 +0800

    Added Motion layout

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -43,7 +43,6 @@ class LoadingButton @JvmOverloads constructor(
                 }
                 valueAnimator.start()
             }
-            ButtonState.Completed -> invalidate()
         }
     }
 
